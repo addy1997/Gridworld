3bb034baf7cf2504f6e24e8c72ee3716403997f0
Curiouscat <adwaitnaik2@gmail.com>
2020-05-20 14:21:22 +0530

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -4,11 +4,26 @@
 # In[ ]:
 
 
-from setuptools import setup
+import setuptools
 
-setup(name='Grid',
-      version='0.0.1',
-      install_requires=['gym']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Grid"
+    version="0.0.1",
+    author="Adwait Naik",
+    author_email="adwaitnaik2@gmail.com",
+    description="Grid world for openai gym",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/addy1997/Grid",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
-
  
