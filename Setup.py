8b44a32d3fa81b6cc0dc56f34be34a58a4cf7630
Adwait P Naik <adwaitnaik2@gmail.com>
2020-06-21 00:04:04 +0530

    error in setup.py fixed

--- Setup.py
+++ Setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Gridworld"
+    name="Gridworld",
     version="0.0.1",
     author="Adwait Naik",
     author_email="adwaitnaik2@gmail.com",
@@ -27,3 +27,26 @@ setuptools.setup(
     python_requires='>=3.6',
 )
  
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
