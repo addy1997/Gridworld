89569502a7b75f661d0f76f39af4f86b208b8fe2
Curiouscat <adwaitnaik2@gmail.com>
2020-06-21 15:15:58 +0530

    test.py updated

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,13 @@
 
 from __future__ import unicode_literals
 
-from envs.GridEnvironment import GridEnv
+import gym 
+import Gridworld
+
+#to make the environment 
+environment = gym.make('Grid-v0')
+while environment.verbose == True:
+	start_ = environment.reset()
+    - = environment.step(env.action_space.sample())
+
 
